f5a99e6a81db554de6644b9a9977abb3c27e1e19
Sverre Hårstadstrand <sverre.harstadstrand@genus.no>
2016-10-25 09:01:44 +0200

    Initial submit

---
+++ SignService/Global.asax.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+
+namespace SignService
+{
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+        }
+    }
+}
